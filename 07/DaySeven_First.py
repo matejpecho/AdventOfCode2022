3c180094bb25ce8cc97d4a6880ad0d8d63dfebb4
Matej Pecho <matej.pecho@gmail.com>
2022-12-08 23:50:19 +0100

    Day seven - first commit

--- 07/DaySeven_First.py
+++ 07/DaySeven_First.py
@@ -1,1 +1,19 @@
-myinput_file = open('./07/myinput.txt', 'r').read().splitlines()
+mypuzzle = open('./07/myinput.txt', 'r').read().splitlines()
+
+terminal_output = []
+path = []
+
+for line in mypuzzle:
+    terminal_output = line.split(' ')
+    if(terminal_output[1] == 'cd'):
+        if(terminal_output[2] == '..'):
+            path.pop()
+        else:
+            path.append(terminal_output[2])
+        print(terminal_output)
+    elif(terminal_output[1] == 'ls'):
+        continue
+    else: 
+        print(terminal_output[0], terminal_output[1])
+
+print(path)
