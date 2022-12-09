a2a998580628264a2b7298f3a724b7eebf63d92c
Matej Pecho <matej.pecho@gmail.com>
2022-12-09 15:56:01 +0100

    Day Seven - First part Completed

--- 07/DaySeven_First.py
+++ 07/DaySeven_First.py
@@ -1,19 +1,60 @@
 mypuzzle = open('./07/myinput.txt', 'r').read().splitlines()
 
 terminal_output = []
+whole_path = []
 path = []
+current_dir_size = 0
 
 for line in mypuzzle:
     terminal_output = line.split(' ')
-    if(terminal_output[1] == 'cd'):
-        if(terminal_output[2] == '..'):
-            path.pop()
-        else:
-            path.append(terminal_output[2])
-        print(terminal_output)
-    elif(terminal_output[1] == 'ls'):
-        continue
-    else: 
-        print(terminal_output[0], terminal_output[1])
 
-print(path)
+    if (terminal_output[0] == '$'):
+        if (terminal_output[1] == 'cd'):
+            if (terminal_output[2] == '..'):
+                whole_path.append(path[-1])
+                path.pop()
+            else:
+
+                current_dir_size = 0
+                path.append([])
+                path[-1].append(terminal_output[2])
+                #path.append(terminal_output[2])
+
+        elif (terminal_output[0] == 'ls'):
+            continue
+    else:
+        if (terminal_output[0] == "dir"):
+            continue
+
+        current_dir_size += int(terminal_output[0])
+
+        if (len(path) > 0):
+            # print(whole_path[0])
+            #print('\n')
+            for dir in path:
+
+                if(len(dir) > 1):
+                    dir[1] = dir[1] + current_dir_size
+                else:
+                    dir.append(current_dir_size)
+                    
+                #print(path)
+                #print(dir)
+                #print('----------------------------')
+        current_dir_size = 0
+        #print(path)
+
+
+print('\nResult:')   
+#print(path)
+for a in path:
+    whole_path.append(a)
+print(whole_path)
+
+size = 0
+
+for b in whole_path:
+    if(b[1] < 100000):
+        size += b[1]
+print(size)
+    #print(terminal_output)
