0f8ce49a51be637148dff8e69bcf65ecde4d1541
Matej Pecho <matej.pecho@gmail.com>
2022-12-13 00:18:47 +0100

    Day Eight - part 2 completed

--- 08/DayEight_Second.py
+++ 08/DayEight_Second.py
@@ -1,63 +1,49 @@
 mypuzzle = open('./08/myinput.txt', 'r').read().splitlines()
 
 grid = []
-count = 0
-
-left = True
-right = True
-up = True
-down = True
 
 for line in mypuzzle:
     grid.append(list(map(int,line)))
-
 
 left_score = 0
 right_score = 0
 up_score = 0
 down_score = 0
+full_score = 0
 
 for r in range(len(grid)):
     for c in range(len(grid[r])):
-        print(grid[r][c])
+        #print(grid[r][c])
+
         for item in range(c-1, -1, -1):
             left_score += 1
             if(grid[r][item] >= grid[r][c]):
-                
-                left = False
                 break
-            left = True
-            
+
         for item in range(c + 1, len(grid[r])):
             right_score += 1
             if(grid[r][item] >= grid[r][c]):
-                right = False
                 break
-            right = True
-        print(right_score)
 
-        for item in range(0, r):
+        for item in range(r-1, -1, -1):
+            up_score += 1
             if(grid[item][c] >= grid[r][c]):
-                up = False
                 break
-            up = True
             
         for item in range(r + 1, len(grid[r])):
+            down_score += 1
             if(grid[item][c] >= grid[r][c]):
-                down = False
                 break
-            down = True
                 
-        if(left or right or up or down):
-            #print('item true: ' + str(grid[r][c]))
-            count += 1
-        left = True
-        right = True
-        up = True
-        down = True
+        if(r == 0 or c == 0 or r == len(grid)-1 or c == len(grid[r]) - 1):
+            #print('edge')
+            left_score = 0
+        if(full_score < (left_score * right_score * up_score * down_score)):
+            full_score = left_score * right_score * up_score * down_score
+
         left_score = 0
         right_score = 0
         up_score = 0
         down_score = 0
  
-print(count)
+print(full_score)
